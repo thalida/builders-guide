46ecd3528d6ac4d144c7c654fce36172b10d4029
Thalida Noel <thalida.c.noel@gmail.com>
2020-09-21 21:39:17 -0400

    Removed all alpha nav things

--- app/src/views/Cookbook/Build/Search.vue
+++ app/src/views/Cookbook/Build/Search.vue
@@ -16,16 +16,11 @@
 
     <div class="search__scroll-container">
       <div v-if="showItems">
-        <ol class="search__content" v-if="renderData.hasItems">
+        <ol class="search__content" v-if="renderData.length > 0">
           <li
             class="search__content__row"
-            v-for="(item, index) in renderData.items"
-            v-observe-visibility="{
-              callback: onVisibilityChanged,
-              intersection: navi.intersectionOptions,
-            }"
-            :key="item.name"
-            :data-letter="item.letter">
+            v-for="(item, index) in renderData"
+            :key="item.name">
 
             <a
               v-if="item.type === 'header'"
@@ -65,30 +60,14 @@
             </div>
           </li>
         </ol>
-        <div class="search__content" v-else>
-          No results
+        <div class="search__content search__content--status" v-else>
+          No items found matching <span class="font-weight--medium">{{inputQuery}}</span>
         </div>
       </div>
-      <div class="search__content" v-else>
-        loading...
+      <div class="search__content search__content--status" v-else>
+        Loading Minecraft {{this.version}} Items...
       </div>
     </div>
-
-    <ol class="search__alpha">
-       <li
-         v-for="letter in navi.alpha"
-         :key="letter">
-         <a
-           v-if="renderData.letters[letter]"
-           :href="`#${letter}`"
-           :class="{'is-selected': navi.selected === letter}"
-           v-on:click="onLetterClick(letter)"
-           v-on:keyup.enter="onLetterClick(letter)">
-           {{letter}}
-         </a>
-         <span v-else>{{letter}}</span>
-       </li>
-   </ol>
 
     <section class="search__action-bar">
       <div class="search__action-bar__inner">
@@ -110,7 +89,6 @@
 </template>
 
 <script>
-import 'intersection-observer'
 import Modal from '@/components/Modal.vue'
 import searchIcon from '../../../components/icons/search.vue'
 import checkIcon from '../../../components/icons/check.vue'
@@ -126,31 +104,18 @@ export default {
     checkIcon,
   },
   data () {
-    const alpha = 'abcdefghijklmnopqrstuvwxyz'.split('')
     const inputQuery = (typeof this.query === 'string' && this.query.length > 0) ? this.query : ''
     return {
-      $scrollContainer: null,
       modalAriaLabel: 'Search Modal',
       inputQuery,
       renderDataByQuery: {},
       showItems: false,
-      navi: {
-        show: false,
-        alpha,
-        inViewport: {},
-        selected: null,
-        fromUserClick: false,
-        arrivedAtLetter: false,
-        // https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API
-        intersectionOptions: {
-          root: null,
-          rootMargin: '0px 0px 0px 0px',
-          threshold: 0,
-        },
-      },
     }
   },
   computed: {
+    version () {
+      return this.$store.state.selectedVersion
+    },
     formattedInputQuery () {
       return this.inputQuery.toLowerCase()
     },
@@ -178,39 +143,12 @@ export default {
     renderData () {
       return this.getRenderDataByQuery(this.inputQuery)
     },
-    focusedLetterInView () {
-      const letters = Object.keys(this.navi.inViewport)
-
-      if (letters.length === 0) {
-        return null
-      }
-
-      letters.sort()
-      const focusedLetter = letters[0]
-
-      if (this.$route.hash.indexOf(focusedLetter) < 0) {
-        this.$router.replace({
-          path: this.$route.path,
-          query: { q: this.inputQuery },
-          hash: focusedLetter
-        })
-      }
-
-      return focusedLetter
-    },
   },
   mounted () {
-    this.$scrollContainer = this.$el.querySelector('.search__scroll-container')
     this.$store.dispatch('setupSearchStore')
-    this.navi.intersectionOptions.root = this.$scrollContainer
-    this.$scrollContainer.addEventListener('scroll', this.onScroll)
     setTimeout(() => {
       this.showItems = true
-      this.scrollToHash()
     }, 0)
-  },
-  destroyed () {
-    document.removeEventListener('scroll', this.onScroll)
   },
   methods: {
     getRenderDataByQuery (query) {
@@ -218,8 +156,8 @@ export default {
       //   return this.renderDataByQuery[query]
       // }
 
-      const renderItems = []
-      const letterHasItems = {}
+      const renderData = []
+      const foundLetters = {}
 
       for (let i = 0, l = this.items.length; i < l; i += 1) {
         const name = this.items[i]
@@ -229,35 +167,26 @@ export default {
           continue
         }
 
-        if (typeof letterHasItems[letter] === 'undefined') {
-          renderItems.push({
+        if (typeof foundLetters[letter] === 'undefined') {
+          renderData.push({
             id: letter,
             letter,
             type: 'header',
             size: 35,
           })
-          letterHasItems[letter] = false
+          foundLetters[letter] = true
         }
 
-        renderItems.push({
+        renderData.push({
           id: name,
           name,
           letter,
           type: 'item',
           size: 40,
         })
-
-        letterHasItems[letter] = true
-      }
-
-      const renderData = {
-        items: renderItems,
-        hasItems: renderItems.length > 0,
-        letters: letterHasItems
       }
 
       // this.renderDataByQuery[query] = renderData
-
       return renderData
     },
     getItemImage (item) {
@@ -283,27 +212,6 @@ export default {
         { isBold: true, value: itemName.substring(boldStart, boldEnd) },
         { isBold: false, value: itemName.substring(boldEnd) },
       ]
-    },
-    scrollToHash () {
-      const hash = this.$route.hash
-
-      if (typeof hash !== 'string' || hash.length === 0) {
-        return
-      }
-
-      const element = this.$scrollContainer.querySelector(hash)
-      if (typeof element === 'undefined' || element === null) {
-        return
-      }
-      const top = element.offsetTop
-
-      if (this.$scrollContainer.scrollTop === top) {
-        this.navi.selected = this.focusedLetterInView
-        this.navi.fromUserClick = false
-        this.navi.arrivedAtLetter = true
-      } else {
-        setTimeout(() => (this.$scrollContainer.scrollTo(0, top)), 0)
-      }
     },
     onScroll () {
       if (this.navi.fromUserClick && !this.navi.arrivedAtLetter) {
@@ -355,8 +263,7 @@ export default {
 
       this.$router.replace({
         path: this.$route.path,
-        query: { q: this.inputQuery },
-        hash: this.focusedLetterInView
+        query: { q: this.inputQuery }
       })
     },
     submit () {
@@ -379,7 +286,8 @@ export default {
 
   .searchbox,
   &__content__row,
-  &__action-bar__inner {
+  &__action-bar__inner,
+  &__content--status {
     width: 80%;
     max-width: 600px;
     margin: 0 auto;
@@ -408,6 +316,12 @@ export default {
     flex-flow: column nowrap;
     align-items: center;
     width: 100%;
+
+    &--status {
+      display: block;
+      text-align: center;
+      font-size: 1.6em;
+    }
   }
 
   &__action-bar {
